6347b7858fe3e882cb32fa5dffe770d493f98358
Oriol Romaní <oromani@Oriols-MacBook-Pro.local>
2021-11-15 11:55:12 +0100

    Update docs description

--- androminacrew/urls.py
+++ androminacrew/urls.py
@@ -21,11 +21,11 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Snippets API",
+        title="Andromina-crew API",
         default_version="v1",
-        description="Test description",
+        description="",
         terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
+        contact=openapi.Contact(email="oriolromanipicas@gmail.com"),
         license=openapi.License(name="BSD License"),
     ),
     public=True,
