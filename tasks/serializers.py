2aadba509dac9af07768d60890b8f08bf8c87c13
Oriol Romaní <oromani@Oriols-MacBook-Pro.local>
2021-11-17 11:16:37 +0100

    Created_at and updated_at fields

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,4 +1,4 @@
-from django.core.exceptions import ValidationError
+import copy
 from rest_framework import serializers
 from .models import Task, WorkTime
 
@@ -8,7 +8,16 @@ class TaskSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Task
-        fields = ["uid", "name", "start_date", "status", "company", "user"]
+        fields = [
+            "uid",
+            "name",
+            "start_date",
+            "status",
+            "company",
+            "user",
+            "created_at",
+            "updated_at",
+        ]
 
     def create(self, validated_data):
         company = self.context.get("company")
@@ -22,12 +31,12 @@ class TaskSerializer(serializers.ModelSerializer):
         return data
 
     def to_internal_value(self, data):
-        try:
-            status_choices = {choice[1]: choice[0] for choice in Task.STATUS_CHOICES}
-            data["status"] = status_choices[data["status"]]
-        except KeyError:
-            raise ValidationError("Status field is not valid")
-        return super().to_internal_value(data)
+        modified_data = copy.deepcopy(data)
+        status_choices = {choice[1]: str(choice[0]) for choice in Task.STATUS_CHOICES}
+        if "status" in modified_data:
+            if modified_data["status"] not in status_choices.values():
+                modified_data["status"] = status_choices.get(modified_data["status"])
+        return super().to_internal_value(modified_data)
 
 
 class WorkTimeSerializer(serializers.ModelSerializer):
